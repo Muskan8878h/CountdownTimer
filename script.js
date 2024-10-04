7e885000d0ca3f49f64472bd005e0aa74b60e2fe
muskan <muskan8878h@gmail.com>
2024-10-04 17:42:49 +0530

    create a function name clock declare end and now  and calculate its difference

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const  endDate= "5 October 2025 12:00 AM"
 document.getElementById("end-date").innerText = endDate;
+
+const inputs = document.querySelectorAll("input");
+
+function clock(){
+    const end =  new Date(endDate);
+    const now= new Date();
+    const diff= (end-now)/1000; //divide  by 1000 to convert into seconds
+    // console.log(end);
+    // console.log(now);
+}
