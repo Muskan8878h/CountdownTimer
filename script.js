5fdab7c58cadaaee438bc405247b37502898c209
muskan <muskan8878h@gmail.com>
2024-10-04 17:44:04 +0530

    convert into days , hours, minutes, seconds

--- script.js
+++ script.js
@@ -9,4 +9,11 @@ function clock(){
     const diff= (end-now)/1000; //divide  by 1000 to convert into seconds
     // console.log(end);
     // console.log(now);
+    
+    if(diff<0) return ; //value will not show in negitive so we use this
+
+    inputs[0].value = Math.floor(diff/3600/24); //convert into days
+    inputs[1].value = Math.floor(diff/3600)%24; //convert into hours
+    inputs[2].value = Math.floor(diff/60)%60;   //convert into minutes
+    inputs[3].value = Math.floor(diff)%60;      //convert into seconds 
 }
